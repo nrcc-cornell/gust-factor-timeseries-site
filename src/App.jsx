2318584261a18a2ac523daddfbef7861eac5623a
Ben Eck <be99@cornell.edu>
2025-12-08 09:20:48 -0500

    basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 export default function App() {
   return (
-    <>
+    <div className='flex flex-col gap-5'>
       <h1>Gust Factor Timeseries</h1>
-      <div className="card">
+      <div className="min-w-sm max-w-7xl w-4/5 min-h-[400px] max-h-[800px] h-[75vh] p-5 mx-auto border border-gray-300 rounded-xl shadow-lg">
         <p>Content</p>
       </div>
-    </>
+    </div>
   )
 }
