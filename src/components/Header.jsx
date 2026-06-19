1b5d117a751b3dbb26dea2f6ce1b7d37469723a6
Ben Eck <be99@cornell.edu>
2026-06-19 08:41:53 -0400

    refactored Nrcc Wrapper to component

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <header>
-      <a href='https://www.nrcc.cornell.edu/'>
-        <img src='/images/banner.png' alt='Northeast Regional Climate Center banner' />
-      </a>
-    </header>
-  );
-}
